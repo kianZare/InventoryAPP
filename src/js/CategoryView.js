fd5a87e5967670aee58267db5b01f80f61863ac3
Kian Zareinajad <kzareinajad@gmail.com>
2023-07-05 01:14:31 +0100

    toggle category section

--- src/js/CategoryView.js
+++ src/js/CategoryView.js
@@ -3,10 +3,19 @@ import Storage from "./Storage.js";
 const categoryTitle = document.querySelector("#category-title");
 const categoryDescription = document.querySelector("#category-description");
 const addNewCategoryBtn = document.querySelector("#add-new-category");
+const toggleAddCategoryBtn = document.getElementById("toggle-add-category");
+const categoryWrapper = document.getElementById("category-wrapper");
+const cancelAddCategoryBtn = document.getElementById("cancel-add-category");
 
 class CategoryView {
   constructor() {
     addNewCategoryBtn.addEventListener("click", (e) => this.addNewCategory(e));
+    toggleAddCategoryBtn.addEventListener("click", (e) =>
+      this.toggleAddCategory(e)
+    );
+    cancelAddCategoryBtn.addEventListener("click", (e) =>
+      this.cancelAddCategory(e)
+    );
     this.categories = [];
   }
   addNewCategory(e) {
@@ -20,6 +29,8 @@ class CategoryView {
     this.createCategoriesList();
     categoryDescription.value = "";
     categoryTitle.value = "";
+    categoryWrapper.classList.add("hidden");
+    toggleAddCategoryBtn.classList.remove("hidden");
   }
   setApp() {
     this.categories = Storage.getAllCategoreies();
@@ -27,14 +38,24 @@ class CategoryView {
   createCategoriesList() {
     let result = `<option class="bg-slate-500 text-slate-300" value="">
     select a category
-  </option>`;
+    </option>`;
     this.categories.forEach((element) => {
       result += `<option class="bg-slate-500 text-slate-300" value=${element.id}>
-    ${element.title}
-  </option>`;
+      ${element.title}
+    </option>`;
     });
     const categoryDOM = document.getElementById("product-category");
     categoryDOM.innerHTML = result;
+  }
+  toggleAddCategory(e) {
+    e.preventDefault();
+    categoryWrapper.classList.remove("hidden");
+    toggleAddCategoryBtn.classList.add("hidden");
+  }
+  cancelAddCategory(e) {
+    e.preventDefault();
+    categoryWrapper.classList.add("hidden");
+    toggleAddCategoryBtn.classList.remove("hidden");
   }
 }
 
